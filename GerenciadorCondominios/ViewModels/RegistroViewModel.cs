aae0bdf6b8bd5f1e46ea4c64c9b416da9b3560ee
lucaskrause <37541622+lucaskrause@users.noreply.github.com>
2020-09-17 09:41:05 -0300

    Alguns arquivos do GerenciadorCondominio

---
+++ GerenciadorCondominios/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        public string Telefone { get; set; }
+
+        public string Foto { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [EmailAddress(ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é Obrigatório")]
+        [StringLength(40, ErrorMessage = "Use menos caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme sua senha")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string SenhaConfirmada { get; set; }
+
+    }
+}
